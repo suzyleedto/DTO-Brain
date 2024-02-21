51c20cd02d27c7a69f5acf17489d82b0904169c2
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-02-21 18:16:25 +0800

    add page app

---
+++ Apps/main_app.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import streamlit as st
+
+with st.echo("below"):
+    from st_pages import Page, add_page_title, show_pages
+
+    "## Declaring the pages in your app:"
+
+    show_pages(
+        [
+            Page("Apps/main_app.py", "Home", "🏠"),
+            # Can use :<icon-name>: or the actual icon
+            Page("Apps/prob_statement.py", "Problem Statement", "✏️"),
+            # The pages appear in the order you pass them
+        ]
+    )
+
+    add_page_title()  # Optional method to add title and icon to current page
