a4bcfc217175048bab3b3d8471ee4aa10fad9029
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-02-21 18:07:47 +0800

    create problem statement app

---
+++ Apps/prob_statement.py
@@ -0,0 +1,47 @@
+ # Importing required packages
+import streamlit as st
+import openai
+import uuid
+import time
+import pandas as pd
+import io
+from openai import OpenAI
+
+# Initialize OpenAI client
+client = OpenAI()
+
+# Your chosen model
+#MODEL = "gpt-3.5-turbo-16k" # Legacy
+MODEL = "gpt-4-turbo-preview" # Latest model
+#MODEL = "gpt-4-1106-preview"
+
+# Initialize session state variables
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+
+if "run" not in st.session_state:
+    st.session_state.run = {"status": None}
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "retry_error" not in st.session_state:
+    st.session_state.retry_error = 0
+    
+    
+st.set_page_config(page_title="Problem Statement here")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Target Audience:")
+    st.subheader("Who's problem are you trying to solve?")
+    txt_who = st.text_area(placeholder="Grocery shoppers")
+   
+
+with col2:
+   st.header("Problem:")
+   st.subheader("Write a short description of the problem")
+   txt_who = st.text_area(placeholder="Find the best deals in online groceries")
+   
+st.button("Generate Problem Statement")
