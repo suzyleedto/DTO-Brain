44cd3658764af01b341c2518e51a4464c3e596e2
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-12 17:46:51 +0800

    add all

--- Apps/scamper.py
+++ Apps/scamper.py
@@ -168,13 +168,13 @@ if st.button("Generate Ideas using SCAMPER"):
                     
             while st.session_state.run.status != 'completed':
                 time.sleep(5)
-                status_c.update(label=f"{st.session_state.run.status}...", state="running")
+                status_a.update(label=f"{st.session_state.run.status}...", state="running")
                 st.session_state.run = openai.beta.threads.runs.retrieve(
                     thread_id=st.session_state.thread.id,
                     run_id=st.session_state.run.id
                 )
             print(st.session_state.run.status)
-            status_c.update(label="Complete", state="complete", expanded=True)
+            status_a.update(label="Complete", state="complete", expanded=True)
             thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
             message_text = thread_messages.data[0].content[0].text.value
             st.markdown("### A-Adapt\n")
@@ -182,4 +182,155 @@ if st.button("Generate Ideas using SCAMPER"):
             st.markdown(message_text)
             st.markdown("\n===========================\n")
                 #st.markdown(message_text)       
-            
+    with tabM:
+            status_m.update(label ="Starting work...", expanded=False, state = "running")
+            if "assistant" not in st.session_state:
+                openai.api_key = st.secrets["OPENAI_API_KEY"]
+                st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT_SCAMPER_M"])
+                st.session_state.thread = client.beta.threads.create(
+                    metadata={'session_id': st.session_state.session_id}
+                )    
+            prompt = "Generate Ideas for this: \""""+txt_who +"\""" and Problem: \""""+ txt_what + " and Product: \""""+ txt_where+ "\""""
+            message_data = {
+                    "thread_id": st.session_state.thread.id,
+                    "role": "user",
+                    "content": prompt
+                }
+
+            st.session_state.messages = client.beta.threads.messages.create(**message_data)
+                
+            st.session_state.run = client.beta.threads.runs.create(
+                thread_id=st.session_state.thread.id,
+                assistant_id=st.session_state.assistant.id,            )
+
+                    
+            while st.session_state.run.status != 'completed':
+                time.sleep(5)
+                status_m.update(label=f"{st.session_state.run.status}...", state="running")
+                st.session_state.run = openai.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread.id,
+                    run_id=st.session_state.run.id
+                )
+            print(st.session_state.run.status)
+            status_m.update(label="Complete", state="complete", expanded=True)
+            thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
+            message_text = thread_messages.data[0].content[0].text.value
+            st.markdown("### M-Modify\n")
+            st.markdown("#### _The modify technique refers to changing the process in a way that unleashes more innovative capabilities or solves problems._\n")
+            st.markdown(message_text)
+            st.markdown("\n===========================\n")
+                #st.markdown(message_text)                  
+    with tabP:
+            status_p.update(label ="Starting work...", expanded=False, state = "running")
+            if "assistant" not in st.session_state:
+                openai.api_key = st.secrets["OPENAI_API_KEY"]
+                st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT_SCAMPER_P"])
+                st.session_state.thread = client.beta.threads.create(
+                    metadata={'session_id': st.session_state.session_id}
+                )    
+            prompt = "Generate Ideas for this: \""""+txt_who +"\""" and Problem: \""""+ txt_what + " and Product: \""""+ txt_where+ "\""""
+            message_data = {
+                    "thread_id": st.session_state.thread.id,
+                    "role": "user",
+                    "content": prompt
+                }
+
+            st.session_state.messages = client.beta.threads.messages.create(**message_data)
+                
+            st.session_state.run = client.beta.threads.runs.create(
+                thread_id=st.session_state.thread.id,
+                assistant_id=st.session_state.assistant.id,            )
+
+                    
+            while st.session_state.run.status != 'completed':
+                time.sleep(5)
+                status_p.update(label=f"{st.session_state.run.status}...", state="running")
+                st.session_state.run = openai.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread.id,
+                    run_id=st.session_state.run.id
+                )
+            print(st.session_state.run.status)
+            status_p.update(label="Complete", state="complete", expanded=True)
+            thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
+            message_text = thread_messages.data[0].content[0].text.value
+            st.markdown("### P-Put to another context\n")
+            st.markdown("#### _This technique concerns how to put the current product or process in another purpose or how to use the existing product to solve problems._\n")
+            st.markdown(message_text)
+            st.markdown("\n===========================\n")
+                #st.markdown(message_text)  
+    with tabE:
+            status_e.update(label ="Starting work...", expanded=False, state = "running")
+            if "assistant" not in st.session_state:
+                openai.api_key = st.secrets["OPENAI_API_KEY"]
+                st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT_SCAMPER_E"])
+                st.session_state.thread = client.beta.threads.create(
+                    metadata={'session_id': st.session_state.session_id}
+                )    
+            prompt = "Generate Ideas for this: \""""+txt_who +"\""" and Problem: \""""+ txt_what + " and Product: \""""+ txt_where+ "\""""
+            message_data = {
+                    "thread_id": st.session_state.thread.id,
+                    "role": "user",
+                    "content": prompt
+                }
+
+            st.session_state.messages = client.beta.threads.messages.create(**message_data)
+                
+            st.session_state.run = client.beta.threads.runs.create(
+                thread_id=st.session_state.thread.id,
+                assistant_id=st.session_state.assistant.id,            )
+
+                    
+            while st.session_state.run.status != 'completed':
+                time.sleep(5)
+                status_e.update(label=f"{st.session_state.run.status}...", state="running")
+                st.session_state.run = openai.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread.id,
+                    run_id=st.session_state.run.id
+                )
+            print(st.session_state.run.status)
+            status_e.update(label="Complete", state="complete", expanded=True)
+            thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
+            message_text = thread_messages.data[0].content[0].text.value
+            st.markdown("### E-Eliminate\n")
+            st.markdown("#### _This technique aims to identify the parts of the process that can be eliminated to improve the process product or service._\n")
+            st.markdown(message_text)
+            st.markdown("\n===========================\n")
+                #st.markdown(message_text)  
+    with tabR:
+            status_r.update(label ="Starting work...", expanded=False, state = "running")
+            if "assistant" not in st.session_state:
+                openai.api_key = st.secrets["OPENAI_API_KEY"]
+                st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT_SCAMPER_R"])
+                st.session_state.thread = client.beta.threads.create(
+                    metadata={'session_id': st.session_state.session_id}
+                )    
+            prompt = "Generate Ideas for this: \""""+txt_who +"\""" and Problem: \""""+ txt_what + " and Product: \""""+ txt_where+ "\""""
+            message_data = {
+                    "thread_id": st.session_state.thread.id,
+                    "role": "user",
+                    "content": prompt
+                }
+
+            st.session_state.messages = client.beta.threads.messages.create(**message_data)
+                
+            st.session_state.run = client.beta.threads.runs.create(
+                thread_id=st.session_state.thread.id,
+                assistant_id=st.session_state.assistant.id,            )
+
+                    
+            while st.session_state.run.status != 'completed':
+                time.sleep(5)
+                status_r.update(label=f"{st.session_state.run.status}...", state="running")
+                st.session_state.run = openai.beta.threads.runs.retrieve(
+                    thread_id=st.session_state.thread.id,
+                    run_id=st.session_state.run.id
+                )
+            print(st.session_state.run.status)
+            status_r.update(label="Complete", state="complete", expanded=True)
+            thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
+            message_text = thread_messages.data[0].content[0].text.value
+            st.markdown("### R-Reverse\n")
+            st.markdown("#### _The reverse or rearrange technique aims to explore the innovative potential when changing the order of the process in the production line._\n")
+            st.markdown(message_text)
+            st.markdown("\n===========================\n")
+                #st.markdown(message_text)  
