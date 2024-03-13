062997bbbee79844ae69505fb183c3b1f701e4a8
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-13 10:56:45 +0800

    updated index

--- Apps/scamper.py
+++ Apps/scamper.py
@@ -138,7 +138,7 @@ if st.button("Generate Ideas using SCAMPER"):
         print(st.session_state.run.status)
         status_c.update(label="Complete", state="complete", expanded=True)
         thread_messages = client.beta.threads.messages.list(st.session_state.thread.id)
-        message_text = thread_messages.data[0].content[1].text.value
+        message_text = thread_messages.data[1].content[0].text.value
         st.markdown("### C-Combine\n")
         st.markdown("#### _The combined technique tends to analyze the possibility of merging two or more ideas, stages of the process or product in one single more efficient output._\n")
         st.markdown(message_text)
