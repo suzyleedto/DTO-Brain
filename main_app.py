ffdd4c0582f3be98467ddbdf888a25f3fb0d6f21
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-13 21:55:33 +0800

    added icon

--- main_app.py
+++ main_app.py
@@ -4,22 +4,22 @@ import streamlit as st
 
 from st_pages import Page, add_page_title, show_pages
 
-
+add_page_title("Welcome to your DTO Toolbox")  # Optional method to add title and icon to current page
 show_pages(
         [
-            Page(path="main_app.py", name = "DTO Toolbox Home"),
+            Page("main_app.py","DTO Toolbox Home",🏠),
             # Can use :<icon-name>: or the actual icon
-            Page(path="pages/prob_statement.py", name="Problem Statement Generator"),
+            Page("pages/prob_statement.py", "Problem Statement Generator","✏️"),
             # The pages appear in the order you pass them
-            Page(path="pages/scamper.py", name="Brainstorming Buddy using SCAMPER"),
+            Page("pages/scamper.py", "Brainstorming Buddy using SCAMPER","💡"),
             # The pages appear in the order you pass them
-            Page(path = "pages/proto_persona.py", name = "Proto-Persona Creator")
+            Page("pages/proto_persona.py","Proto-Persona Creator","🧑‍🤝‍🧑"),
             # The pages appear in the order you pass them
             
         ]
     )
 
-add_page_title("Welcome to your DTO Toolbox")  # Optional method to add title and icon to current page
+
 st.page_link("main_app.py", label="Home", icon="🏠")
 st.page_link("pages/prob_statement.py", label="Problem Statement Generator", icon="✏️")
 st.page_link("pages/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡")
