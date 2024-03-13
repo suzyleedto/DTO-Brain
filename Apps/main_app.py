00e668f73b6cb188eeff1f9013dbe0e519b51cc7
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-13 14:09:57 +0800

    updated menu

--- Apps/main_app.py
+++ Apps/main_app.py
@@ -17,6 +17,6 @@ show_pages(
     )
 
 add_page_title("Welcome to your DTO Toolbox")  # Optional method to add title and icon to current page
-
-st.page_link("Apps/prob_statement.py", label="Problem Statement Generator", icon="✏️")
-st.page_link("Apps/scamper.py", "Brainstorming Buddy using SCAMPER", icon="💡", disabled=True)
+st.page_link("Apps/main_app.py", label="Home", icon="🏠")
+st.page_link("Apps/prob_statement.py", label="Problem Statement Generator", icon="✏️",disabled=True)
+st.page_link("Apps/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡", disabled=True)
