bffb756a8e38acdd68bc465a06145a1d7cff74ea
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-13 15:01:42 +0800

    updated path

--- main_app.py
+++ main_app.py
@@ -9,14 +9,14 @@ show_pages(
         [
             Page("main_app.py", "DTO Toolbox Home", "🛠️"),
             # Can use :<icon-name>: or the actual icon
-            Page("Apps/prob_statement.py", "Problem Statement Generator", "✏️"),
+            Page("pages/prob_statement.py", "Problem Statement Generator", "✏️"),
             # The pages appear in the order you pass them
-            Page("Apps/scamper.py", "Brainstorming Buddy using SCAMPER", "💡")
+            Page("pages/scamper.py", "Brainstorming Buddy using SCAMPER", "💡")
             # The pages appear in the order you pass them
         ]
     )
 
 add_page_title("Welcome to your DTO Toolbox")  # Optional method to add title and icon to current page
 st.page_link("main_app.py", label="Home", icon="🏠")
-st.page_link("Apps/prob_statement.py", label="Problem Statement Generator", icon="✏️",disabled=True)
-st.page_link("Apps/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡", disabled=True)
+st.page_link("pages/prob_statement.py", label="Problem Statement Generator", icon="✏️",disabled=True)
+st.page_link("pages/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡", disabled=True)
