6c47d5afeabb7b3af781bc53f253192cc01511f0
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-13 14:56:32 +0800

    update

--- pages/main_app.py
+++ pages/main_app.py
@@ -7,7 +7,7 @@ from st_pages import Page, add_page_title, show_pages
 
 show_pages(
         [
-            Page("Apps/main_app.py", "DTO Toolbox Home", "🛠️"),
+            Page("main_app.py", "DTO Toolbox Home", "🛠️"),
             # Can use :<icon-name>: or the actual icon
             Page("Apps/prob_statement.py", "Problem Statement Generator", "✏️"),
             # The pages appear in the order you pass them
@@ -17,6 +17,6 @@ show_pages(
     )
 
 add_page_title("Welcome to your DTO Toolbox")  # Optional method to add title and icon to current page
-st.page_link("Apps/main_app.py", label="Home", icon="🏠")
+st.page_link("main_app.py", label="Home", icon="🏠")
 st.page_link("Apps/prob_statement.py", label="Problem Statement Generator", icon="✏️",disabled=True)
 st.page_link("Apps/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡", disabled=True)
