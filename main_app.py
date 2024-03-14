e19b60baa2d5c0c66d872002b8c01799bd6f115a
suzyleeph <118877367+suzyleeph@users.noreply.github.com>
2024-03-14 11:50:36 +0800

    changed toolbox icon

--- main_app.py
+++ main_app.py
@@ -14,7 +14,7 @@ from st_pages import Page, add_page_title, show_pages
 add_page_title()
 show_pages(
         [
-            Page("main_app.py","DTO Toolbox Home","🏠"),
+            Page("main_app.py","DTO Toolbox Home","🛠️"),
             # Can use :<icon-name>: or the actual icon
             Page("apps/prob_statement.py", "Problem Statement Generator","✏️"),
             # The pages appear in the order you pass them
@@ -27,7 +27,7 @@ show_pages(
     )
 
 
-st.page_link("main_app.py", label="Home", icon="🏠")
+st.page_link("main_app.py", label="Home", icon="🛠️")
 st.page_link("apps/prob_statement.py", label="Problem Statement Generator", icon="✏️")
 st.page_link("apps/scamper.py", label = "Brainstorming Buddy using SCAMPER", icon="💡")
 st.page_link("apps/proto_persona.py", label = "Proto-Persona Creator", icon  = "🧑‍🤝‍🧑")
